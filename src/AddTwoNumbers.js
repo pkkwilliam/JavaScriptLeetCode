dc3940d2ebc0ffe98d7ff9d4f8958f2691a0bf34
Ka Kei Pun <pkkwilliam@Kas-MacBook-Pro.local>
2019-05-31 23:13:24 -0400

    add jasmine

---
+++ src/AddTwoNumbers.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * Ka Kei Pun
+ * 
+ * 2. Add Two Numbers
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    
+};
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+let node = ListNode(5);
+console.log(node.val);
